9288b68acaa33fee8f144e8e162c460ea6af946e
Hassam Aziz <118983911+Hassamaziz@users.noreply.github.com>
2023-12-10 22:35:46 +0500

    Update Button.js

--- components/button/Button.js
+++ components/button/Button.js
@@ -20,7 +20,7 @@ export default function Button(props) {
         redirect: 'follow'
       };
       
-      fetch("http://localhost:3000/api/appointments", requestOptions)
+      fetch("https://hackathon-app-1g4z.vercel.app/api/appointments", requestOptions)
         .then(response => response.text())
         .then(result => console.log(result))
         .catch(error => console.log('error', error));
