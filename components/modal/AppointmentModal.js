e95143a9ada1a418d4a4f87039fc4b0f29771ba0
Hassam Aziz <118983911+Hassamaziz@users.noreply.github.com>
2023-12-10 22:27:46 +0500

    Update AppointmentModal.js

--- components/modal/AppointmentModal.js
+++ components/modal/AppointmentModal.js
@@ -32,7 +32,7 @@ var requestOptions = {
   redirect: 'follow'
 };
 
-await fetch("https://hackathon-app-eight.vercel.app/api/appointments", requestOptions)
+await fetch("https://hackathon-app-1g4z.vercel.app/api/appointments", requestOptions)
  
     } catch (error) {
       console.error("Error adding product:", error.message);
